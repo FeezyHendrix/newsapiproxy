9b2ddda6ab41bcb244b5b3de85702bbe567aea6c
AbdulHafeez AbdulRaheem <switchdevelop1@gmail.com>
2022-04-05 09:24:34 +0100

    feat: update

--- cacheClient.js
+++ cacheClient.js
@@ -1,9 +1,14 @@
 import { createClient } from "redis";
 
-let globalClient = null;
 
 export const getRedisClient = async () => {
-  const client = createClient();
+  const client = createClient({
+    url: process.env.REDIS_URL,
+    socket: {
+      tls: true,
+      rejectUnauthorized: false,
+    }
+  });
 
   client.on("error", (err) => console.log("Redis Client Error", err));
 
